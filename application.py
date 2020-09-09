1056b577cbd18727c9f8428f5bafad5fc3264513
Jean Villete <jean.villete@gmail.com>
2020-09-08 22:25:31 -0300

    teste e ajuste

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1>olá Jean FIAP!</h1>\nVersao deployada via BeanStalk! o/"
+    return "<h1>ola Jean FIAP!</h1>\nVersao deployada via BeanStalk! o/ \n mostrando que nao se pode utilizar caracteres especiais"
 
 if __name__ == '__main__':
     application.run()
